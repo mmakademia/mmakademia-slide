38f0f33df0a0fdf91abb55c9e9bb22eebb250d98
lorant Nemeth <calvez@1780488.no-reply.drupal.org>
2016-03-03 11:41:23 +0100

    working example

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+var sassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+gulp.task('sass', function() {
+  return gulp.src('scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+});
+
+// Starts a test server, which you can view at http://localhost:8079
+gulp.task('server', ['build'], function() {
+  gulp.src('./')
+    .pipe($.webserver({
+      port: 8079,
+      host: 'localhost',
+      fallback: 'index.html',
+      livereload: true,
+      open: true
+    }))
+  ;
+});
+
